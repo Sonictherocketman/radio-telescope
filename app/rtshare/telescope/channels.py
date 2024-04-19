e0f42ffb501d1ce1ddd4c0b5e29218e0b97a033f
Brian Schrader <brian@brianschrader.com>
2024-04-19 16:54:53 -0700

    Adds some debugging for channel auth + missign migration

--- app/rtshare/telescope/channels.py
+++ app/rtshare/telescope/channels.py
@@ -7,22 +7,14 @@ from .models import Telescope
 
 class TelescopeChannelManager(DefaultChannelManager):
 
-    def get_channels_for_request(self, request, view_kwargs):
-        self.request = request
-        authentication = TokenAuthentication()
-        try:
-            request.user, request.auth = authentication.authenticate(request)
-        except AuthenticationFailed:
-            pass
-        return super().get_channels_for_request(request, view_kwargs)
-
-    def can_read_channel(self, _, channel):
+    def can_read_channel(self, user, channel):
+        print(user)
         ids = [
             str(id)
             for id in Telescope.objects.all().values_list('id', flat=True)
         ]
         return (
-            self.request.user.is_authenticated
+            user.is_authenticated
             and
             str(channel) in ids
         )
