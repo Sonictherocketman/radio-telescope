21c5ffb1e449ddede149c808c10f6de9b651d8f2
Brian Schrader <brian@brianschrader.com>
2026-05-02 18:28:39 -0700

    Does not remove center peak

--- astronomer/astronomer/workers/spectrum.py
+++ astronomer/astronomer/workers/spectrum.py
@@ -68,21 +68,21 @@ def plot_to_image(values, freq, observation, buff_percent, y_scale_factor=2):
         logger.debug(f'Using NTF: {f.name}')
 
         # TODO: Temp hack to remove DC offset spike
-        if observation.calibration:
-            identifier = observation.calibration.identifier
-        else:
-            identifier = default_identifier
-
-        signal_buffer = signal_buffers[identifier]
-        if len(signal_buffer.get_data()) > 1:
-            prevous_values = signal_buffer.get_data()[1]
-            l = len(values)
-            center = l // 2
-            width = 10
-            values[center-width:center+width] = (
-                values[center-width:center+width]
-                / (prevous_values[center-width:center+width] * signal_buffer.length)
-            )
+#         if observation.calibration:
+#             identifier = observation.calibration.identifier
+#         else:
+#             identifier = default_identifier
+#
+#         signal_buffer = signal_buffers[identifier]
+#         if len(signal_buffer.get_data()) > 1:
+#             prevous_values = signal_buffer.get_data()[1]
+#             l = len(values)
+#             center = l // 2
+#             width = 10
+#             values[center-width:center+width] = (
+#                 values[center-width:center+width]
+#                 / (prevous_values[center-width:center+width] * signal_buffer.length)
+#             )
         # END HACK
 
         title = observation.identifier
